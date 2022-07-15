17b6cac4d266bd66122a3a7ad33b6fbc7ccebfcf
khumbulani <kerkhood9@gmail.com>
2022-07-15 10:55:01 +0200

    app.js index.html added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+     <nav>
+  <ul>
+    <li><strong>Home</strong></li>
+    <li><a href="about.html">About Us</a></li>
+    <li><a href="clients.html">Our Clients</a></li>
+    <li><a href="products.html">Our Products</a></li>
+    <li><a href="services.html">Our Services</a></li>
+    <li><a href="contact.html">Contact Us</a></li>
+  </ul>
+</nav>
     </div>
   );
 }
