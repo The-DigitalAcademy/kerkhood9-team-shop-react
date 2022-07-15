bfb1c3abb9ec214eaeb478475e31f8ed45013514
khumbulani <kerkhood9@gmail.com>
2022-07-15 11:49:25 +0200

    index.css index.html

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <nav>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Pricing</a></li>
-                    <li><a href="#">Terms of use</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </nav>
+   
     </div>
   );
 }
